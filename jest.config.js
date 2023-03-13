1d35d5238a66e77421f5f40a32ad317b09264eef
Jack Crompton <git@straightouttacrompton.dev>
2023-03-13 22:17:15 +0100

    Add configs and format files

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    moduleFileExtensions: ["js", "json", "ts"],
+    rootDir: "src",
+    testRegex: ".*\\.spec\\.ts$",
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest",
+    },
+    collectCoverageFrom: ["**/*.(t|j)s"],
+    coverageDirectory: "../coverage",
+    testEnvironment: "node",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { API_PORT } from './consts';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { API_PORT } from "./consts";
 
 async function bootstrap() {
-  console.log(`Server is listening on port ${API_PORT}\n`);
+    console.log(`Server is listening on port ${API_PORT}\n`);
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(API_PORT);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(API_PORT);
 }
 bootstrap();
