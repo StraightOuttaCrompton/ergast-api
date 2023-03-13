d649f15de67a46c8fe077a104f0f0c5314826c20
Jack Crompton <git@straightouttacrompton.dev>
2023-03-13 22:04:55 +0100

    Add API_PORT const

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { API_PORT } from './consts';
 
 async function bootstrap() {
+  console.log(`Server is listening on port ${API_PORT}\n`);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(API_PORT);
 }
 bootstrap();
