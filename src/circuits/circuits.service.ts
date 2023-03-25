2294d04468e8f531bca3957d26c9d0e2d1d55f84
Jack Crompton <git@straightouttacrompton.dev>
2023-03-25 16:26:21 +0100

    Migrate /circuits to use prisma.$queryRaw

--- src/circuits/circuits.service.ts
+++ src/circuits/circuits.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { circuits } from "@prisma/client";
+import { circuits, Prisma } from "@prisma/client";
 import { DEFAULT_LIMIT, DEFAULT_OFFSET } from "../consts";
 import { PrismaService } from "../prisma.service";
 import { GetCircuitsDto } from "./dto/get-circuits.dto";
@@ -21,77 +21,73 @@ export class CircuitsService {
         grid,
         fastest,
     }: GetCircuitsDto) {
-        const circuits = await this.prisma.circuits.findMany({
-            take: limit,
-            skip: offset,
-            orderBy: {
-                circuitRef: "asc",
-            },
+        const circuits = (await this.prisma.$queryRaw`
+            SELECT DISTINCT circuits.circuitRef, circuits.name, circuits.location, circuits.country, circuits.lat, circuits.lng, circuits.alt, circuits.url 
+            FROM circuits 
+            ${
+                year !== undefined ||
+                driverId !== undefined ||
+                constructorId !== undefined ||
+                status !== undefined ||
+                grid !== undefined ||
+                fastest !== undefined ||
+                result !== undefined
+                    ? Prisma.sql`, races`
+                    : Prisma.empty
+            }
+            ${
+                driverId !== undefined ||
+                constructorId !== undefined ||
+                status !== undefined ||
+                grid !== undefined ||
+                fastest !== undefined ||
+                result !== undefined
+                    ? Prisma.sql`, results`
+                    : Prisma.empty
+            }
+            ${driverId !== undefined ? Prisma.sql`, drivers` : Prisma.empty}
+            ${constructorId !== undefined ? Prisma.sql`, constructors` : Prisma.empty}
 
-            where: {
-                races: {
-                    some: {
-                        year: year
-                            ? {
-                                  // TODO: allow for multiple
-                                  in: [year],
-                              }
-                            : undefined,
-                        round: round
-                            ? {
-                                  // TODO: allow for multiple
-                                  in: [round],
-                              }
-                            : undefined,
-
-                        results: {
-                            some: {
-                                driver: {
-                                    driverRef: driverId
-                                        ? {
-                                              // TODO: allow for multiple
-                                              in: [driverId],
-                                          }
-                                        : undefined,
-                                },
-                                constructorRelation: {
-                                    constructorRef: constructorId
-                                        ? {
-                                              // TODO: allow for multiple
-                                              in: [constructorId],
-                                          }
-                                        : undefined,
-                                },
-                                statusId: status
-                                    ? {
-                                          // TODO: allow for multiple
-                                          in: [status],
-                                      }
-                                    : undefined,
-                                positionText: result?.toString()
-                                    ? {
-                                          // TODO: allow for multiple
-                                          in: [result.toString()],
-                                      }
-                                    : undefined,
-                                grid: grid
-                                    ? {
-                                          // TODO: allow for multiple
-                                          in: [grid],
-                                      }
-                                    : undefined,
-                                rank: fastest
-                                    ? {
-                                          // TODO: allow for multiple
-                                          in: [fastest],
-                                      }
-                                    : undefined,
-                            },
-                        },
-                    },
-                },
-            },
-        });
+            WHERE TRUE 
+            ${
+                year !== undefined ||
+                driverId !== undefined ||
+                constructorId !== undefined ||
+                status !== undefined ||
+                grid !== undefined ||
+                fastest !== undefined ||
+                result !== undefined
+                    ? Prisma.sql`AND races.circuitId=circuits.circuitId`
+                    : Prisma.empty
+            }
+            ${
+                driverId !== undefined ||
+                constructorId !== undefined ||
+                status !== undefined ||
+                grid !== undefined ||
+                fastest !== undefined ||
+                result !== undefined
+                    ? Prisma.sql`AND results.raceId=races.raceId`
+                    : Prisma.empty
+            }
+            ${
+                constructorId !== undefined
+                    ? Prisma.sql`AND results.constructorId=constructors.constructorId AND constructors.constructorRef=${constructorId}`
+                    : Prisma.empty
+            }
+            ${
+                driverId !== undefined
+                    ? Prisma.sql`AND results.driverId=drivers.driverId AND drivers.driverRef=${driverId}`
+                    : Prisma.empty
+            }
+            ${status !== undefined ? Prisma.sql`AND results.statusId=${status}` : Prisma.empty}
+            ${grid !== undefined ? Prisma.sql`AND results.grid=${grid}` : Prisma.empty}
+            ${fastest !== undefined ? Prisma.sql`AND results.rank=${fastest}` : Prisma.empty}
+            ${result !== undefined ? Prisma.sql`AND results.positionText=${result}` : Prisma.empty}
+            ${round !== undefined ? Prisma.sql`AND races.round=${round}` : Prisma.empty}
+            ${year !== undefined ? Prisma.sql`AND races.year=${year}` : Prisma.empty}
+            ORDER BY circuits.circuitRef LIMIT ${offset}, ${limit}
+        `) as circuits[];
 
         return circuits.map(this.formatCircuit);
     }
@@ -110,9 +106,9 @@ export class CircuitsService {
             url: circuit.url,
             circuitName: circuit.name,
             Location: {
-                // TODO: don't default to 0 here
-                lat: (circuit.lat || 0).toString() || "",
-                long: (circuit.lng || 0).toString() || "",
+                // TODO: don't default to 0 here. Remove toPrecision and number cast
+                lat: (+(circuit.lat || 0).toPrecision(6)).toString() || "",
+                long: (+(circuit.lng || 0).toPrecision(6)).toString() || "",
                 // alt: circuit.alt != null ? circuit.alt.toString() : "N/D", // TODO: include
                 locality: circuit.location || "",
                 country: circuit.country || "",
