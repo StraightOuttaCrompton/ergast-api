325c0616a3203df2042c29e51a01ae1a836b668a
Jack Crompton <git@straightouttacrompton.dev>
2023-04-01 21:18:10 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { API_PORT } from "./consts";
 import { mainConfig } from "./main.config";
@@ -8,6 +9,32 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
     mainConfig(app);
+
+    const config = new DocumentBuilder()
+        .setTitle("Cats example")
+        .setDescription("The cats API description")
+        .setVersion("1.0")
+        .addTag("cats")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(
+        "api",
+        app,
+        document
+        // {
+        //     explorer?: boolean;
+        //     swaggerOptions?: Record<string, any>;
+        //     customCss?: string;
+        //     customCssUrl?: string;
+        //     customJs?: string;
+        //     customfavIcon?: string;
+        //     swaggerUrl?: string;
+        //     customSiteTitle?: string;
+        //     validatorUrl?: string;
+        //     url?: string;
+        //     urls?: Record<'url' | 'name', string>[];
+        // }
+    );
 
     await app.listen(API_PORT);
 }
