86f03ebd3317bf71b455bcf0184d4e79d0c97c37
Jack Crompton <git@straightouttacrompton.dev>
2023-04-02 18:23:40 +0200

    Remove logging from prisma.service

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,20 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
-        super({ log: process.env.NODE_ENV === "production" ? [] : ["query", "info", "warn", "error"] });
-    }
-
     async onModuleInit() {
-        if (process.env.NODE_ENV !== "production") {
-            // @ts-ignore
-            this.$on("query", (e: any) => {
-                console.log("Query: " + e.query);
-                console.log("Params: " + e.params);
-                console.log("Duration: " + e.duration + "ms");
-            });
-        }
-
         await this.$connect();
     }
 
