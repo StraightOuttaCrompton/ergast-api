b51c99a8a1adcbf5329bc4c33422931f81b9807a
Jack Crompton <git@straightouttacrompton.dev>
2023-04-02 17:14:50 +0200

    Add main.swagger

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,15 @@
 import { NestFactory } from "@nestjs/core";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-import { API_PORT, API_VERSION, ERGAST_DESCRIPTION } from "./consts";
+import { API_PORT } from "./consts";
 import { mainConfig } from "./main.config";
+import { mainSwagger } from "./main.swagger";
 
 async function bootstrap() {
     console.log(`Server is listening on port ${API_PORT}\n`);
 
     const app = await NestFactory.create(AppModule);
     mainConfig(app);
-
-    const config = new DocumentBuilder()
-        .setTitle("Ergast")
-        .setDescription(ERGAST_DESCRIPTION)
-        .setVersion(API_VERSION)
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup("", app, document, {
-        // explorer?: boolean;
-        // swaggerOptions?: Record<string, any>;
-        // customCss?: string;
-        // customCssUrl?: string;
-        // customJs?: string;
-        // customfavIcon?: string;
-        // swaggerUrl?: string;
-        customSiteTitle: "Ergast docs",
-        // validatorUrl?: string;
-        // url?: string;
-        // urls?: Record<'url' | 'name', string>[];
-    });
+    mainSwagger(app);
 
     await app.listen(API_PORT);
 }
