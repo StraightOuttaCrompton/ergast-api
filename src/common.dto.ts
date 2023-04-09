2e3100b754793f884ebd11542bcf49662f0f597d
Jack Crompton <git@straightouttacrompton.dev>
2023-04-09 14:04:53 +0200

    Add min to offset validation pipe

--- src/common.dto.ts
+++ src/common.dto.ts
@@ -1,11 +1,12 @@
 import { Type } from "class-transformer";
-import { IsInt, IsOptional, IsPositive } from "class-validator";
+import { IsInt, IsOptional, IsPositive, Min } from "class-validator";
 import { DEFAULT_LIMIT, DEFAULT_OFFSET } from "./consts";
 
 export class CommonDto {
     @Type(() => Number)
     @IsOptional()
     @IsInt()
+    @Min(0)
     offset?: number = DEFAULT_OFFSET;
 
     @Type(() => Number)
