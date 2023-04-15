6e7e4536d6bf10a557b011eef0413e2a8af07732
Jack Crompton <git@straightouttacrompton.dev>
2023-04-15 22:12:08 +0200

    Add SITE_TITLE

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@ export const API_PORT = 3000;
 export const DEFAULT_LIMIT = 30;
 export const DEFAULT_OFFSET = 0;
 
-export const ERGAST_DESCRIPTION =
+export const SITE_TITLE = "Ergast API";
+export const SITE_DESCRIPTION =
     "The Ergast API provides a historical record of motor racing data for non-commercial purposes. The API provides data for the Formula One series, from the beginning of the world championships in 1950.";
 export const API_VERSION = "1.0";
