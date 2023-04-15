9e84e5741fc7e73dd2c77c51439497be49f19f0b
Jack Crompton <git@straightouttacrompton.dev>
2023-04-16 00:19:54 +0200

    addd results formatting

--- src/formatters/formatResult.ts
+++ src/formatters/formatResult.ts
@@ -1,18 +1,180 @@
-import { results } from "@prisma/client";
+import { results, status } from "@prisma/client";
+import formatCircuit from "./formatCircuit";
+import { Result, ResultsResponse } from "../responseDtos/Results.dto";
+import { Circuit } from "src/responseDtos/Circuit.dto";
+import formatDriver from "./formatDriver";
+import formatConstructor from "./formatConstructor";
 
-export default function formatResult(circuit: results) {
-    return;
-    // return {
-    //     circuitId: circuit.circuitRef,
-    //     url: circuit.url,
-    //     circuitName: circuit.name,
-    //     Location: {
-    //         // TODO: don't default to 0 here. Remove toPrecision and number cast
-    //         lat: (+(circuit.lat || 0).toPrecision(6)).toString() || "",
-    //         long: (+(circuit.lng || 0).toPrecision(6)).toString() || "",
-    //         // alt: circuit.alt != null ? circuit.alt.toString() : "N/D", // TODO: include
-    //         locality: circuit.location || "",
-    //         country: circuit.country || "",
-    //     },
-    // };
+function formatResult(
+    response: results &
+        status & {
+            year: number;
+            round: number;
+            raceName: string;
+            raceDate: Date;
+            raceTime: Date | null;
+            raceUrl: string | null;
+        } & {
+            circuitRef: string;
+            circuitName: string;
+            circuitLocation: string;
+            circuitCountry: string;
+            circuitUrl: string;
+            circuitLat: number;
+            circuitLng: number;
+            circuitAlt: number;
+        } & {
+            driverRef: string;
+            driverNumber: number | null;
+            driverCode: string | null;
+            driverForename: string;
+            driverSurname: string;
+            driverDob: Date;
+            driverNationality: string;
+            driverUrl: string;
+        } & {
+            constructorRef: string;
+            constructorName: string;
+            constructorNationality: string;
+            constructorUrl: string;
+        }
+) {
+    return new Result({
+        number: response.number.toString(),
+        position: response.position?.toString(),
+        positionText: response.positionText,
+        points: response.points.toString(),
+        Driver: formatDriver({
+            driverRef: response.driverRef,
+            number: response.number,
+            code: response.driverCode,
+            url: response.raceUrl,
+            forename: response.driverForename,
+            surname: response.driverSurname,
+            dob: response.driverDob,
+            nationality: response.driverNationality,
+        }),
+        Constructor: formatConstructor({
+            constructorRef: response.constructorRef,
+            url: response.constructorUrl,
+            name: response.constructorName,
+            nationality: response.constructorNationality,
+        }),
+        grid: response.grid.toString(),
+        laps: response.laps.toString(),
+        status: response.status,
+        Time: {
+            millis: response.milliseconds?.toString(),
+            time: response.time,
+        },
+    });
+}
+
+export default function formatResultResponse(
+    response: (results &
+        status & {
+            year: number;
+            round: number;
+            raceName: string;
+            raceDate: Date;
+            raceTime: Date | null;
+            raceUrl: string | null;
+        } & {
+            circuitRef: string;
+            circuitName: string;
+            circuitLocation: string;
+            circuitCountry: string;
+            circuitUrl: string;
+            circuitLat: number;
+            circuitLng: number;
+            circuitAlt: number;
+        } & {
+            driverRef: string;
+            driverNumber: number | null;
+            driverCode: string | null;
+            driverForename: string;
+            driverSurname: string;
+            driverDob: Date;
+            driverNationality: string;
+            driverUrl: string;
+        } & {
+            constructorRef: string;
+            constructorName: string;
+            constructorNationality: string;
+            constructorUrl: string;
+        })[]
+) {
+    const resultsResponse = extractByYearAndRound(response, formatResult);
+
+    return resultsResponse.map((response) => new ResultsResponse(response));
+}
+
+function extractByYearAndRound<S, T>(
+    response: (S & {
+        year: number;
+        round: number;
+        raceUrl: string;
+        raceName: string;
+        circuitRef: string;
+        circuitName: string;
+        circuitLocation: string;
+        circuitCountry: string;
+        circuitUrl: string;
+        circuitLat: number;
+        circuitLng: number;
+        circuitAlt: number;
+    })[],
+    formatResult: (response: S) => T
+) {
+    const responseMap: Record<
+        string,
+        { season: string; round: string; url: string; raceName: string; Circuit: Circuit; Results: T[] }
+    > = {};
+
+    for (let i = 0; i < response.length; i++) {
+        const {
+            year,
+            round,
+            raceUrl,
+            raceName,
+            circuitRef,
+            circuitName,
+            circuitLocation,
+            circuitCountry,
+            circuitUrl,
+            circuitLat,
+            circuitLng,
+            circuitAlt,
+        } = response[i];
+
+        // Generate a unique key for this year and round
+        const key = `${year}-${round}`;
+
+        const formattedResults = formatResult(response[i]);
+
+        if (!responseMap[key]) {
+            // Create a new standings object if one does not exist for this season and round
+            responseMap[key] = {
+                season: year.toString(),
+                round: round.toString(),
+                url: raceUrl,
+                raceName,
+                Circuit: formatCircuit({
+                    circuitRef,
+                    name: circuitName,
+                    location: circuitLocation,
+                    country: circuitCountry,
+                    url: circuitUrl,
+                    lat: circuitLat,
+                    lng: circuitLng,
+                    alt: circuitAlt,
+                }),
+                Results: [formattedResults],
+            };
+        } else {
+            responseMap[key].Results.push(formattedResults);
+        }
+    }
+
+    return Object.values(responseMap);
 }
--- src/routes/results/results.service.ts
+++ src/routes/results/results.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { Prisma, results } from "@prisma/client";
+import { Prisma, results, status } from "@prisma/client";
 import { DEFAULT_LIMIT, DEFAULT_OFFSET } from "../../consts";
 import * as sql from "../../sql";
 import { GetResultsDto } from "./dto/get-results.dto";
-import formatResult from "../../formatters/formatResult";
+import formatResultResponse from "../../formatters/formatResult";
 import prisma from "../../prisma";
 
 @Injectable()
@@ -22,10 +22,10 @@ export class ResultsService {
     }: GetResultsDto) {
         const sqlResponse = (await prisma.$queryRaw`
             SELECT 
-            races.year AS 'raceYear', races.round AS 'raceRound', races.name AS 'raceName', DATE_FORMAT(races.date, '%Y-%m-%d') AS 'raceDate', DATE_FORMAT(races.time, '%H:%i:%S') AS 'raceTime', races.url AS 'raceUrl', 
-            circuits.circuitRef, circuits.name, circuits.location, circuits.country, circuits.url, circuits.lat, circuits.lng, circuits.alt,
+            races.year AS 'year', races.round AS 'round', races.name AS 'raceName', DATE_FORMAT(races.date, '%Y-%m-%d') AS 'raceDate', DATE_FORMAT(races.time, '%H:%i:%S') AS 'raceTime', races.url AS 'raceUrl', 
+            circuits.circuitRef, circuits.name AS 'circuitName', circuits.location AS 'circuitLocation', circuits.country AS 'circuitCountry', circuits.url AS 'circuitUrl', circuits.lat AS 'circuitLat', circuits.lng AS 'circuitLng', circuits.alt AS 'circuitAlt',
             results.*,
-            drivers.driverRef, drivers.number 'driverNumber', drivers.code, drivers.forename, drivers.surname, DATE_FORMAT(drivers.dob, '%Y-%m-%d') AS 'dob', drivers.nationality, drivers.url AS 'driverUrl',
+            drivers.driverRef, drivers.number AS 'driverNumber', drivers.code AS 'driverCode', drivers.forename AS 'driverForename', drivers.surname AS 'driverSurname', drivers.dob AS 'driverDob', drivers.nationality AS 'driverNationality', drivers.url AS 'driverUrl',
             status.statusId, status.status,
             constructors.constructorRef, constructors.name AS 'constructorName', constructors.nationality AS 'constructorNationality', constructors.url AS 'constructorUrl'
             
@@ -49,44 +49,62 @@ export class ResultsService {
             ${sql.and.fastest({ fastest })}
 
             ORDER BY races.year, races.round, results.positionOrder LIMIT ${offset}, ${limit}
-        `) as results[] & {
-            raceName: string;
-            raceDate: Date;
-            raceTime: Date | null;
-            raceUrl: string | null;
-        };
+        `) as (results &
+            status & {
+                year: number;
+                round: number;
+                raceName: string;
+                raceDate: Date;
+                raceTime: Date | null;
+                raceUrl: string | null;
+            } & {
+                circuitRef: string;
+                circuitName: string;
+                circuitLocation: string;
+                circuitCountry: string;
+                circuitUrl: string;
+                circuitLat: number;
+                circuitLng: number;
+                circuitAlt: number;
+            } & {
+                driverRef: string;
+                driverNumber: number | null;
+                driverCode: string | null;
+                driverForename: string;
+                driverSurname: string;
+                driverDob: Date;
+                driverNationality: string;
+                driverUrl: string;
+            } & {
+                constructorRef: string;
+                constructorName: string;
+                constructorNationality: string;
+                constructorUrl: string;
+            })[];
 
-        // return sqlResponse.map(formatResult);
-
-        return sqlResponse;
+        return formatResultResponse(sqlResponse);
     }
 
     async getResult(year: number, round: number) {
-        const sqlResponse = (await prisma.$queryRaw`
-            SELECT results.year, results.round, 
-            results.name AS 'raceName', 
-            results.url AS 'raceURL', 
-            
-            DATE_FORMAT(results.date, '%Y-%m-%d') AS 'date', DATE_FORMAT(results.time, '%H:%i:%S') AS 'time', 
-            DATE_FORMAT(results.fp1_date, '%Y-%m-%d') AS 'fp1_date', DATE_FORMAT(results.fp1_time, '%H:%i:%S') AS 'fp1_time', 
-            DATE_FORMAT(results.fp2_date, '%Y-%m-%d') AS 'fp2_date', DATE_FORMAT(results.fp2_time, '%H:%i:%S') AS 'fp2_time', 
-            DATE_FORMAT(results.fp3_date, '%Y-%m-%d') AS 'fp3_date', DATE_FORMAT(results.fp3_time, '%H:%i:%S') AS 'fp3_time', 
-            DATE_FORMAT(results.quali_date, '%Y-%m-%d') AS 'quali_date', DATE_FORMAT(results.quali_time, '%H:%i:%S') AS 'quali_time', 
-            DATE_FORMAT(results.sprint_date, '%Y-%m-%d') AS 'sprint_date', DATE_FORMAT(results.sprint_time, '%H:%i:%S') AS 'sprint_time', 
-            
-            circuits.*
-            FROM results, circuits
-
-            WHERE results.circuitId=circuits.circuitId
-
-            ${sql.and.year({ year })}
-            ${sql.and.round({ round })}
-        `) as results[];
-
-        if (!sqlResponse.length) {
-            throw new NotFoundException();
-        }
-
-        return sqlResponse.map(formatResult)[0];
+        // const sqlResponse = (await prisma.$queryRaw`
+        //     SELECT results.year, results.round,
+        //     results.name AS 'raceName',
+        //     results.url AS 'raceURL',
+        //     DATE_FORMAT(results.date, '%Y-%m-%d') AS 'date', DATE_FORMAT(results.time, '%H:%i:%S') AS 'time',
+        //     DATE_FORMAT(results.fp1_date, '%Y-%m-%d') AS 'fp1_date', DATE_FORMAT(results.fp1_time, '%H:%i:%S') AS 'fp1_time',
+        //     DATE_FORMAT(results.fp2_date, '%Y-%m-%d') AS 'fp2_date', DATE_FORMAT(results.fp2_time, '%H:%i:%S') AS 'fp2_time',
+        //     DATE_FORMAT(results.fp3_date, '%Y-%m-%d') AS 'fp3_date', DATE_FORMAT(results.fp3_time, '%H:%i:%S') AS 'fp3_time',
+        //     DATE_FORMAT(results.quali_date, '%Y-%m-%d') AS 'quali_date', DATE_FORMAT(results.quali_time, '%H:%i:%S') AS 'quali_time',
+        //     DATE_FORMAT(results.sprint_date, '%Y-%m-%d') AS 'sprint_date', DATE_FORMAT(results.sprint_time, '%H:%i:%S') AS 'sprint_time',
+        //     circuits.*
+        //     FROM results, circuits
+        //     WHERE results.circuitId=circuits.circuitId
+        //     ${sql.and.year({ year })}
+        //     ${sql.and.round({ round })}
+        // `) as results[];
+        // if (!sqlResponse.length) {
+        //     throw new NotFoundException();
+        // }
+        // return sqlResponse.map(formatResult)[0];
     }
 }
