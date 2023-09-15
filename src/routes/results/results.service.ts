36ce31b8986685771f077e7b6a426d41bbd9a5f2
Jack Crompton <git@straightouttacrompton.dev>
2023-09-15 18:54:27 +0200

    temp

--- src/routes/results/results.service.ts
+++ src/routes/results/results.service.ts
@@ -86,25 +86,30 @@ export class ResultsService {
     }
 
     async getResult(year: number, round: number) {
-        // const sqlResponse = (await prisma.$queryRaw`
-        //     SELECT results.year, results.round,
-        //     results.name AS 'raceName',
-        //     results.url AS 'raceURL',
-        //     DATE_FORMAT(results.date, '%Y-%m-%d') AS 'date', DATE_FORMAT(results.time, '%H:%i:%S') AS 'time',
-        //     DATE_FORMAT(results.fp1_date, '%Y-%m-%d') AS 'fp1_date', DATE_FORMAT(results.fp1_time, '%H:%i:%S') AS 'fp1_time',
-        //     DATE_FORMAT(results.fp2_date, '%Y-%m-%d') AS 'fp2_date', DATE_FORMAT(results.fp2_time, '%H:%i:%S') AS 'fp2_time',
-        //     DATE_FORMAT(results.fp3_date, '%Y-%m-%d') AS 'fp3_date', DATE_FORMAT(results.fp3_time, '%H:%i:%S') AS 'fp3_time',
-        //     DATE_FORMAT(results.quali_date, '%Y-%m-%d') AS 'quali_date', DATE_FORMAT(results.quali_time, '%H:%i:%S') AS 'quali_time',
-        //     DATE_FORMAT(results.sprint_date, '%Y-%m-%d') AS 'sprint_date', DATE_FORMAT(results.sprint_time, '%H:%i:%S') AS 'sprint_time',
-        //     circuits.*
-        //     FROM results, circuits
-        //     WHERE results.circuitId=circuits.circuitId
-        //     ${sql.and.year({ year })}
-        //     ${sql.and.round({ round })}
-        // `) as results[];
-        // if (!sqlResponse.length) {
-        //     throw new NotFoundException();
-        // }
-        // return sqlResponse.map(formatResult)[0];
+        const sqlResponse = (await prisma.$queryRaw`
+            SELECT 
+            races.year AS 'year', races.round AS 'round', races.name AS 'raceName', DATE_FORMAT(races.date, '%Y-%m-%d') AS 'raceDate', DATE_FORMAT(races.time, '%H:%i:%S') AS 'raceTime', races.url AS 'raceUrl', 
+            circuits.circuitRef, circuits.name AS 'circuitName', circuits.location AS 'circuitLocation', circuits.country AS 'circuitCountry', circuits.url AS 'circuitUrl', circuits.lat AS 'circuitLat', circuits.lng AS 'circuitLng', circuits.alt AS 'circuitAlt',
+            results.*,
+            drivers.driverRef, drivers.number AS 'driverNumber', drivers.code AS 'driverCode', drivers.forename AS 'driverForename', drivers.surname AS 'driverSurname', drivers.dob AS 'driverDob', drivers.nationality AS 'driverNationality', drivers.url AS 'driverUrl',
+            status.statusId, status.status,
+            constructors.constructorRef, constructors.name AS 'constructorName', constructors.nationality AS 'constructorNationality', constructors.url AS 'constructorUrl'
+            
+            FROM races, circuits, results, drivers, constructors, status
+            
+            WHERE races.circuitId=circuits.circuitId 
+            AND races.raceId=results.raceId 
+            AND results.driverId=drivers.driverId 
+            AND results.constructorId=constructors.constructorId 
+            AND results.statusId=status.statusId
+   
+            ${sql.and.year({ year })}
+            ${sql.and.round({ round })}
+        `) as results[];
+
+        if (!sqlResponse.length) {
+            throw new NotFoundException();
+        }
+        return sqlResponse.map(formatRe)[0];
     }
 }
